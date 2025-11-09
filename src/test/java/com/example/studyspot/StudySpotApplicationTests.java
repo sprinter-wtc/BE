fbfdf3a1e602d34c8b5da08594a7ced449a6917e
Woojae-Jeong <woo6388@naver.com>
2025-11-10 00:40:43 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/example/studyspot/StudySpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
