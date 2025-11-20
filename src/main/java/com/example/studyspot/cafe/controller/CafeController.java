5fb1ea93655a2062412773b8e6554e50bfb3831e
Woojae-Jeong <woo6388@naver.com>
2025-11-21 06:27:36 +0900

    feat(CafeController): 추천 카페리스트 조회 API 구현

--- src/main/java/com/example/studyspot/cafe/controller/CafeController.java
+++ src/main/java/com/example/studyspot/cafe/controller/CafeController.java
@@ -1,7 +1,9 @@
 package com.example.studyspot.cafe.controller;
 
 import com.example.studyspot.cafe.dto.CafeDetailsDTO;
+import com.example.studyspot.cafe.dto.CafeSimpleInfoDTO;
 import com.example.studyspot.cafe.dto.response.CafeDetailsResponse;
+import com.example.studyspot.cafe.dto.response.RecommendationCafeResponse;
 import com.example.studyspot.cafe.service.CafeService;
 import com.example.studyspot.common.api.ResponseEntityGenerator;
 import com.example.studyspot.common.api.SuccessBody;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/cafes")
 @RestController
@@ -25,5 +29,12 @@ public class CafeController {
     ) {
         CafeDetailsDTO details = cafeService.getCafeDetails(cafeId);
         return ResponseEntityGenerator.success(CafeDetailsResponse.from(details), HttpStatus.OK);
+    }
+
+    @GetMapping("/recommended")
+    public ResponseEntity<SuccessBody<RecommendationCafeResponse>> getRecommendationCafes (
+    ) {
+        List<CafeSimpleInfoDTO> recommendationCafes = cafeService.getRecommendationCafes();
+        return ResponseEntityGenerator.success(RecommendationCafeResponse.from(recommendationCafes), HttpStatus.OK);
     }
 }
