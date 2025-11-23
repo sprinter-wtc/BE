3ea63e1080dea4267af0027897a1b309a3dcb025
Woojae-Jeong <woo6388@naver.com>
2025-11-23 17:08:47 +0900

    feat(User): User 엔티티에 ssaid 필드 추가

--- src/main/java/com/example/studyspot/user/domain/model/User.java
+++ src/main/java/com/example/studyspot/user/domain/model/User.java
@@ -10,21 +10,25 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name="uuser")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String ssaid;
 
     @Embedded
     @AttributeOverride(name = "value", column = @Column(name = "name", unique = true, nullable = false))
     private Name name;
 
     @Embedded
-    @AttributeOverride(name = "value", column = @Column(name = "image_url", unique = true, nullable = false))
+    @AttributeOverride(name = "value", column = @Column(name = "image_url"))
     private ImageUrl imageUrl;
 
-    public static User createUserOfRandomName() {
+    public static User createUserOfRandomName(String ssaid) {
         String name = RandomNameGenerator.generate();
-        return new User(null, new Name(name), null);
+        return new User(null, ssaid, new Name(name), null);
     }
 }
