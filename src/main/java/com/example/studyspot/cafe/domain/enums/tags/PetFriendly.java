b3d02676708d8613a5b018febfee2ebb20741128
Woojae-Jeong <woo6388@naver.com>
2025-11-24 01:23:56 +0900

    refactor(enum): enum value값 수정
    FE와 enum 한글 데이터를 맞추기 위해서 enum의 value 수정

--- src/main/java/com/example/studyspot/cafe/domain/enums/tags/PetFriendly.java
+++ src/main/java/com/example/studyspot/cafe/domain/enums/tags/PetFriendly.java
@@ -3,8 +3,8 @@ package com.example.studyspot.cafe.domain.enums.tags;
 import com.example.studyspot.cafe.domain.enums.BaseEnum;
 
 public enum PetFriendly implements BaseEnum {
-    ALLOWED("애견 동반 가능"),
-    NOT_ALLOWED("애견 동반 불가능");
+    ALLOWED("애견동반 가능"),
+    NOT_ALLOWED("애견동반 불가능");
 
     private final String value;
 
--- src/main/java/com/example/studyspot/cafe/domain/enums/tags/StayDurationPolicy.java
+++ src/main/java/com/example/studyspot/cafe/domain/enums/tags/StayDurationPolicy.java
@@ -3,8 +3,8 @@ package com.example.studyspot.cafe.domain.enums.tags;
 import com.example.studyspot.cafe.domain.enums.BaseEnum;
 
 public enum StayDurationPolicy implements BaseEnum {
-    LIMITED("있음"),
-    UNLIMITED("없음");
+    LIMITED("시간제한 있음"),
+    UNLIMITED("시간제한 없음");
 
     private final String value;
 
